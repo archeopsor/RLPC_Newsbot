aa0645545b92abfc01feb280888a79188892b14b
archeopsor <simirsingh@gmail.com>
2020-08-02 20:20:44 -0400

    typo fix

--- RLPC_Newsbot.py
+++ RLPC_Newsbot.py
@@ -130,7 +130,7 @@ async def on_message(message):
         descriptors = ["have taken down","have defeated","beat","were victorious over", "thwarted", "have upset", "have overpowered", "got the better of", "overcame", "triumphed over"]
         
         print("Reached if True")
-        if team2_rating - team1_rating != 0: #> 70:
+        if team2_rating - team1_rating > 70:
             message = f"""**(TESTING, THIS MAY BE INCORRECT) UPSET ALERT**
 {team1} {team1_record} {choice(descriptors)} {team2} {team2_record} with a score of {team1_score} - {team2_score}
             """
