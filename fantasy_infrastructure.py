748557646c49a5ef6f51f1a061349881175f0e06
archeopsor <simirsingh@gmail.com>
2021-06-26 19:21:47 -0400

    Finish rewriting fantasy_infrastructure (untested)

--- fantasy_infrastructure.py
+++ fantasy_infrastructure.py
@@ -28,14 +28,15 @@ class FantasyHandler:
         """
         fantasy = self.session.fantasy
         lb = pd.Series(dtype="float64")
-        cursor = fantasy.find({"points": {"$gt": 0}}).sort("points", -1) # Get all accounts with more than 0 points
+        cursor = fantasy.find({"points": {"$gt": 0}}).sort(
+            "points", -1)  # Get all accounts with more than 0 points
 
         for i in range(fantasy.count_documents({"points": {"$gt": 0}})):
             info = cursor.next()
             lb[info['username']] = info['points']
 
         return lb
- 
+
     def pick_player(self, discord_id: str, player: str) -> str:
         """Adds an RLPC player to a fantasy account
 
@@ -46,7 +47,7 @@ class FantasyHandler:
         Returns:
             str: Success or error message to send in discord.
         """
-        
+
         # Ensure that it's a valid time to make transfers
         if datetime.now(tz=pytz.timezone("US/Eastern")).weekday() in [1, 3]:
             if datetime.now(tz=pytz.timezone("US/Eastern")).time().hour > 16:
@@ -54,15 +55,16 @@ class FantasyHandler:
         elif datetime.now(tz=pytz.timezone("US/Eastern")).weekday() in [2, 4]:
             if datetime.now(tz=pytz.timezone("US/Eastern")).time().hour < 10:
                 return "You're not allowed to make transfers right now, probably because there are games currently happening or the previous games have not yet been entered into the database. Please contact arco if you think this is an error."
-        
+
         fantasy = self.session.fantasy
         players = self.session.players
         account: dict = fantasy.find_one({"discord_id": discord_id})
-        if not account: 
+        if not account:
             return f"You don't currently have an account! Use {prefix}new to make an account"
 
         # Try to get player (case insensitive), and return if player doesn't exist
-        player_info = self.session.players.find_one({'$text': {'$search': player}})
+        player_info = self.session.players.find_one(
+            {'$text': {'$search': player}})
         if not player_info:
             return "That player couldn't be found in the database. Make sure you spelled their name correctly"
 
@@ -75,7 +77,8 @@ class FantasyHandler:
             return "This player is not available to be picked."
 
         # Make sure this doesn't exceed salary cap
-        new_salary = account['salary'] + player_info['fantasy']['fantasy_value']
+        new_salary = account['salary'] + \
+            player_info['fantasy']['fantasy_value']
         if new_salary > self.SALARY_CAP:
             return f"This player would cause you to exceed the salary cap of {self.SALARY_CAP}. Please choose a different player, or drop someone on your team."
 
@@ -85,30 +88,34 @@ class FantasyHandler:
             return "You already have this player on your team!"
 
         # Add player to list
-        self.session.fantasy.update_one({'_id': account['_id']}, {'$push': {'players': player_info['_id']}})
-    
+        self.session.fantasy.update_one({'_id': account['_id']}, {
+                                        '$push': {'players': player_info['_id']}})
+
         # Add player to player_history (Even if there's an entry there for this player already)
         history = {
-            "Player": player_info['_id'],    
+            "Player": player_info['_id'],
             "Date in": datetime.now(tz=pytz.timezone("US/Eastern")).date(),
             "Date out": None,
             "Points": 0,
         }
-        self.session.fantasy.update_one({"_id": account['_id']}, {'$push': {'player_history': history}})
+        self.session.fantasy.update_one({"_id": account['_id']}, {
+                                        '$push': {'player_history': history}})
 
         # Add transaction to transaction_log
         transaction = {
             "Timestamp": datetime.now(tz=pytz.timezone("US/Eastern")),
-            "Type": "in", 
+            "Type": "in",
             "Player": {
                 "id": player_info['_id'],
                 "salary": player_info['fantasy']['salary'],
             },
         }
-        self.session.fantasy.update_one({"_id": account['_id']}, {'$push': {'transfer_log': transaction}})
-        
+        self.session.fantasy.update_one({"_id": account['_id']}, {
+                                        '$push': {'transfer_log': transaction}})
+
         # Change salary
-        self.session.fantasy.update_one({'_id': account['_id']}, {'$set': {"salary": new_salary}})
+        self.session.fantasy.update_one({'_id': account['_id']}, {
+                                        '$set': {"salary": new_salary}})
 
         return f'Success! {player} has been added to your team. You have {self.SALARY_CAP - new_salary} left before you reach the salary cap.'
 
@@ -118,30 +125,34 @@ class FantasyHandler:
 
         # Make sure account exists
         account: dict = fantasy.find_one({"discord_id": discord_id})
-        if not account: 
+        if not account:
             return f"You don't currently have an account! Use {prefix}new to make an account"
 
         # Make sure player is actually on the team
-        player_info = self.session.players.find_one({'$text': {'$search': player}})
+        player_info = self.session.players.find_one(
+            {'$text': {'$search': player}})
         if player_info['_id'] not in account['players']:
             return f"{player} isn't on your team!"
         else:
-            points = [x for x in account['player_history'] if x['Player'] == player_info['_id']][-1]['Points']
+            points = [x for x in account['player_history']
+                      if x['Player'] == player_info['_id']][-1]['Points']
 
         # Remove player from list
-        self.session.fantasy.update_one({'_id': account['_id']}, {'$pull': {'players': player_info['_id']}})
+        self.session.fantasy.update_one({'_id': account['_id']}, {
+                                        '$pull': {'players': player_info['_id']}})
 
         # Add transaction to transaction_log
         transaction = {
             "Timestamp": datetime.now(tz=pytz.timezone("US/Eastern")),
-            "Type": "out", 
+            "Type": "out",
             "Player": {
                 "id": player_info['_id'],
                 "salary": player_info['fantasy']['salary'],
                 "points": points
             },
         }
-        self.session.fantasy.update_one({"_id": account['_id']}, {'$push': {'transfer_log': transaction}})
+        self.session.fantasy.update_one({"_id": account['_id']}, {
+                                        '$push': {'transfer_log': transaction}})
 
         # Update player's info in player_history
         updates = {
@@ -149,11 +160,13 @@ class FantasyHandler:
                 'player_history.Date out': datetime.now(tz=pytz.timezone("US/Eastern")).date()
             }
         }
-        self.session.fantasy.find_one_and_update({'_id': account['_id'], 'player_history.Player': player_info['_id']}, updates)
+        self.session.fantasy.find_one_and_update(
+            {'_id': account['_id'], 'player_history.Player': player_info['_id']}, updates)
 
         # Change salary
         new_salary = self.SALARY_CAP - player_info['fantasy']['fantasy_value']
-        self.session.fantasy.find_one_and_update({'_id': account['_id']}, {'$set': {'salary': new_salary}})
+        self.session.fantasy.find_one_and_update(
+            {'_id': account['_id']}, {'$set': {'salary': new_salary}})
 
         return f'Success! {player} has been dropped from your team. You have {self.SALARY_CAP - new_salary} left before you reach the salary cap.'
 
@@ -184,7 +197,8 @@ class FantasyHandler:
             return
 
         if pg:
-            doc['fantasy']['fantasy_points'] = round(doc['fantasy']['fantasy_points'] / doc['stats']['general']['Series Played'], 1)
+            doc['fantasy']['fantasy_points'] = round(
+                doc['fantasy']['fantasy_points'] / doc['stats']['general']['Series Played'], 1)
 
         return doc
 
@@ -202,15 +216,15 @@ class FantasyHandler:
         Returns:
             list: List of dictionaries with player information
         """
-        if team.casefold() in ["all","none","no","idc"]:
+        if team.casefold() in ["all", "none", "no", "idc"]:
             team = "all"
-        if league.casefold() in ["all","none","no","idc"]:
+        if league.casefold() in ["all", "none", "no", "idc"]:
             league = "all"
-        if league.casefold() not in ['major','aaa','aa','a','independent', 'maverick', 'indy', 'mav', 'all']:
+        if league.casefold() not in ['major', 'aaa', 'aa', 'a', 'independent', 'maverick', 'indy', 'mav', 'all']:
             return("League could not be understood")
         else:
             league = leagues[league.lower()]
-        
+
         filter = {
             "fantasy.fantasy_value": {'$gte': minsalary, '$lte': maxsalary}
         }
@@ -221,7 +235,7 @@ class FantasyHandler:
             filter['info.team'] = teamIds[team.title()]
         if name != "none":
             filter['username'] = name
-    
+
         # Get 5 (or fewer) random players from the query
         count = self.session.players.count_documents(filter)
         cursor = self.session.players.find(filter)
@@ -232,41 +246,39 @@ class FantasyHandler:
 
         return players
 
-    def player_lb(self, league: str = None, sortby: str = "Fantasy Points", num: int = 10, pergame: bool = False) -> list:
+    def player_lb(self, league: str = None, sortby: str = "fantasy_points", num: int = 10, pergame: bool = False) -> pd.Series:
+        """Returns rlpc players sorted by a stat, usually fantasy points
+
+        Args:
+            league (str, optional): Which league to include players from.. Defaults to None.
+            sortby (str, optional): Which field to sort by. Defaults to "fantasy_points".
+            num (int, optional): How many players to return. Defaults to 10.
+            pergame (bool, optional): Whether to divide by the number of games played. Defaults to False.
+
+        Returns:
+            pd.Series: Sorted series with Username: value pairs.
+        """
         filter = {}
         if league:
             filter['info.league'] = leagues[league.lower()]
-        
-        players = self.session.players.find(filter).sort()
-        # TODO: Finish this
 
-        
-    
-# def player_lb(league: str = None, sortby: str="Fantasy Points", num: int=10, pergame: bool=False) -> pd.DataFrame:
-#     """
-#     Sorts the list of rlpc players
+        players = self.session.players.find(filter)
+        count = self.session.players.count_documents(filter)
+        lb = pd.Series(dtype="float64")
+        for i in range(count):
+            player = players.next()
 
-#     Parameters
-#     ----------
-#     league : str, optional
-#         Which league to include players from. The default is None.
-#     sortby : str, optional
-#         Which column to sort by. The default is "Fantasy Points".
-#     num : int, optional
-#         How many players to return. The default is 10.
+            if sortby.lower() in ['fantasy_points', 'fantasy points']:
+                doc = player['fantasy']
+            else:
+                doc = player['stats']
 
-#     Returns
-#     -------
-#     Sorted dataframe of RLPC players.
+            if pergame:
+                stat = round(doc[sortby] / player['stats']['Games Played'], 1)
+            else:
+                stat = doc[sortby]
 
-#     """
-#     players = select("players").set_index("Username")
-    
-#     if league != None:
-#         players = players[players['League'].str.lower() == league.casefold()]
-    
-#     if pergame:
-#         players['Fantasy Points'] = round(players['Fantasy Points']/players['Series Played'], 1).fillna(value=0)
-#     lb = players['Fantasy Points'].sort_values(ascending=False)
-        
-#     return lb.head(num)
+            lb[player['username']] = stat
+
+        lb.sort_values(ascending=False, inplace=True)
+        return lb.head(num)
