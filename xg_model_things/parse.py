d17c1adc180da1dc55a2e9c844609bf23e9c6aee
Simcha Singh <simchars@email.unc.edu>
2021-09-03 09:41:55 -0400

    increase upset threshold

---
+++ xg_model_things/parse.py
@@ -0,0 +1,15 @@
+from boxcars_py import parse_replay
+
+def parse(file_path: str) -> dict:
+    with open(file_path, "rb") as f:
+        buf = f.read()
+    replay = parse_replay(buf)
+    return replay
+
+def test():
+    parse(r'C:\Users\Simi\Documents\My Games\Rocket League\TAGame\Demos\CA6509C8416BF65001C3A99F3501B356.replay')
+
+if __name__ == "__main__":
+    import timeit
+    num = 10
+    print(timeit.timeit(stmt=test, number=num) / num)
